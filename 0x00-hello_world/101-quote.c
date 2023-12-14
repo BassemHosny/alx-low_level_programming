7bfb29dcdbdc413406f8f455034c586d13cc95ff
BassemHosny <basemhosny255@gmail.com>
2023-12-14 11:48:21 -0800

    my commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,12 +5,12 @@
  *
  * Description : print a qoute using write function
  *
- * Return: 1 (not success)
+ * Return: 1 (error).
 */
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1.quo.59);
+	write(1. quo. 59);
 	return (1);
 }
